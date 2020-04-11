02795d82d9e90fbef158e2337802e878830ce030
ranjithgopalan <59782019+ranjithgopalan@users.noreply.github.com>
2020-04-11 17:19:37 -0400

    Created New application

--- Appus/src/main/java/Ranjith/Appus/ImageProcessingApplication.java
+++ Appus/src/main/java/Ranjith/Appus/ImageProcessingApplication.java
@@ -3,6 +3,7 @@ package Ranjith.Appus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 
 @SpringBootApplication
-public class ImageProcessingApplication {
+public class ImageProcessingApplication extends SpringBootServletInitializer {
 	@RequestMapping("/YourName")
 	String home() {
 		return "Ranjith";
