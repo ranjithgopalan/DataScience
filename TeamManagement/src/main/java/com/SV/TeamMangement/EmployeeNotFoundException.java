a8f2e4a5a89ac3640aa7cbcb7990c7226695d5d4
ranjithgopalan <59782019+ranjithgopalan@users.noreply.github.com>
2020-04-18 10:54:45 -0400

    Adding project TeamManagmentUI

--- TeamManagement/src/main/java/com/SV/TeamMangement/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.SV.TeamMangement;
-
-public class EmployeeNotFoundException extends RuntimeException{
-
-    EmployeeNotFoundException(long id){
-        super("Could not find employee " + id);
-    }
-}
--- TeamManagement/src/test/java/com/SV/TeamMangement/EmployeeEntityTest.java
+++
@@ -1,7 +0,0 @@
-package com.SV.TeamMangement;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EmployeeEntityTest {
-
-}
