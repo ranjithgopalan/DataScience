939707ea273f4cd10a2db26ae829c167730554c5
ranjithgopalan <59782019+ranjithgopalan@users.noreply.github.com>
2020-04-30 17:41:23 -0400

    UI appliation in Django

---
+++ analytics_project/ClinicalData/services.py
@@ -0,0 +1,10 @@
+# from django.contrib.sites import requests
+#
+#
+# def get_team():
+#     url = 'http://http://localhost/EmployeeList'
+#     params = {'ID': {id}}
+#     r = requests.get(url, params=params)
+#     Employee = r.json()
+#     Employee_list = {'Employee':Employee['results']}
+#     return Employee_list
