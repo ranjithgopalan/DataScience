e91765fb36691ab12c6dfdf8e8302fb45ad17431
Ranjith Gopalan <ranjith.gopalan@gmail.com>
2024-01-24 16:45:55 -0500

    Create Django  react project for image validation

---
+++ ImageValidation_Projects/imagevalidation/imagevalidation_controller/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImagevalidationControllerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imagevalidation_controller'
